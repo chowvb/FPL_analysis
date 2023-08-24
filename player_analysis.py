b7526bb923e8261e828f6d25c721d3d902ddb671
Vincent Chow <chowvk8@gmail.com>
2023-08-24 22:01:08 +0100

    debugged economy, player_efficiency and player_contribution functions

--- player_analysis.py
+++ player_analysis.py
@@ -104,8 +104,8 @@ def economy(player_id):
     players_raw_csv = pd.read_csv("data/2023-2024/players_raw.csv")
     player_df = players_raw_csv[players_raw_csv["id"] == int(player_id)].reset_index()
     total_points = float(player_df.at[0, "total_points"])
-    player_value = (player_df.at[0, "value"]/ 10)
-    player_economy = total_points / player_value
+    player_value = (player_df.at[0, "now_cost"]/ 10)
+    player_economy = round(total_points / player_value, 2)
     return player_economy
 
 def percentage_matches_started(player_id):
@@ -125,22 +125,58 @@ def player_efficiency(player_id):
     xa = player_df.at[0, "expected_assists"]
     assists = player_df.at[0, "assists"]
 
-    goals_efficiency = goals / xg
-    assists_efficiency = assists / xa
+    try:
+        goals_efficiency = round((goals / xg), 2)
+    except:
+        goals_efficiency = 0
     
+    try:
+        assists_efficiency = round((assists / xa), 2)
+    except:
+        assists_efficiency = 0
+        
     return goals_efficiency, assists_efficiency
 
 def player_contribution(player_id):
-    from clean_data import scrape_and_clean_player_data
     from team_analysis import copy_premier_league_table
 
-    primary_df, secondary_df = scrape_and_clean_player_data(url + "bootstrap-static/")
+    team_replace_dict = {"team_id": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+                         "Team": 
+                         ["Arsenal",
+                            "Aston Villa",
+                            "Bournemouth",
+                            "Brentford",
+                            "Brighton",
+                            "Burnley",
+                            "Chelsea",
+                            "Crystal Palace", 
+                            "Everton",
+                            "Fulham",
+                            "Liverpool",
+                            "Luton",
+                            "Man City",
+                            "Man Utd",
+                            "Newcastle",
+                            "Nott'm Forrest",
+                            "Sheffield Utd",
+                            "Spurs",
+                            "West Ham",
+                            "Wolves"]
+    }
+    team_dict_df = pd.DataFrame(team_replace_dict)
+
+    primary_df = pd.read_csv("data/2023-2024/players_raw.csv")
     primary_df = primary_df[primary_df["id"] == player_id].reset_index(drop = True)
-    goal_involvement = primary_df.at[0, "goals"] + primary_df.at[0, "assists"]
+    goal_involvement = primary_df.at[0, "goals_scored"] + primary_df.at[0, "assists"]
     
     table_df = copy_premier_league_table()
-    table_df = table_df[table_df["Team"] == primary_df.at[0, "team"]].reset_index(drop = True)
+    table_df = pd.merge(table_df, team_dict_df, on = "Team")
+    table_df = table_df[table_df["team_id"] == primary_df.at[0, "team"]].reset_index(drop = True)
     team_goals = table_df.at[0, "F"]
     
-    percent_goal_involvement = (int(goal_involvement) / int(team_goals)) * 100
+    try :
+        percent_goal_involvement = (int(goal_involvement) / int(team_goals)) * 100
+    except:
+        percent_goal_involvement = 0 
+
     return percent_goal_involvement
