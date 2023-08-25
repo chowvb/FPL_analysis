41811a86afc656acd9a800f6d099301a300e9acc
Vincent Chow <chowvk8@gmail.com>
2023-08-25 18:36:55 +0100

    Simplified scrap_and_clean_player_data() to just scrape_data() applications of the previous function was redundant. Added get game week data for individuals and all players as well as extended stats scraping

--- clean_data.py
+++ clean_data.py
@@ -2,168 +2,107 @@ import csv, requests, json
 import pandas as pd
 import numpy as np 
 import os
-
-def scrape_and_clean_player_data():
-    # Define columns to delete and seperated into a seperate DataFrame
-    other_columns = {
-        "old_names": ["id", "first_name","second_name", "web_name", "team", "element_type", "chance_of_playing_next_round", "chance_of_playing_this_round","team_code","transfers_in", "transfers_in_event",
-                "transfers_out", "transfers_out_event", "now_cost_rank", "now_cost_rank_type", "form_rank", "form_rank_type","points_per_game_rank",
-                "points_per_game_rank_type","code", "cost_change_event", "cost_change_event_fall", "dreamteam_count", "in_dreamteam", "news", "news_added",
-                "photo", "special", "squad_number", "status", "corners_and_indirect_freekicks_text", "direct_freekicks_text", "penalties_text", "selected_rank",
-                "selected_rank_type", "influence_rank", "influence_rank_type", "creativity_rank", "creativity_rank_type", "threat_rank", "threat_rank_type",
-                "ict_index_rank", "ict_index_rank_type", "corners_and_indirect_freekicks_order", "direct_freekicks_order", "penalties_order"],
-        
-        "new_names" : ["id", "first_name","second_name", "web_name", "team", "position", "chance_of_playing_next_round", "chance_of_playing_this_round","team_code","transfers_in", "transfers_in_event",
-                "transfers_out", "transfers_out_event", "now_cost_rank", "now_cost_rank_type", "form_rank", "form_rank_type","points_per_game_rank",
-                "points_per_game_rank_type","code", "cost_change_event", "cost_change_event_fall", "dreamteam_count", "in_dreamteam", "news", "news_added",
-                "photo", "special", "squad_number", "status", "corners_and_indirect_freekicks_text", "direct_freekicks_text", "penalties_text", "selected_rank",
-                "selected_rank_type", "influence_rank", "influence_rank_type", "creativity_rank", "creativity_rank_type", "threat_rank", "threat_rank_type",
-                "ict_index_rank", "ict_index_rank_type", "set_piece_order", "direct_freekicks_order", "penalties_order"]
-
-    }
-
-    # Define columns to be kept in the main dataframe, and any amendments to column names to easier to understand titles.
-    df_columns = {
-        "old_names": ["id", "first_name","second_name", "web_name", "team", "element_type", "selected_by_percent", "now_cost","cost_change_start", "cost_change_start_fall",
-                    "value_form", "value_season", "total_points", "event_points", "bps", "points_per_game", "ep_next", "ep_this", "form", "starts", "minutes",
-                    "goals_scored", "expected_goals", "assists", "expected_assists", "expected_goal_involvements", "clean_sheets", "goals_conceded", "expected_goals_conceded",
-                    "own_goals", "penalties_saved", "penalties_missed", "yellow_cards", "red_cards", "saves", "bonus", "influence", "creativity", "threat", "ict_index",
-                    "expected_goals_per_90", "expected_assists_per_90", "clean_sheets_per_90", "expected_goal_involvements_per_90", "expected_goals_conceded_per_90", 
-                    "goals_conceded_per_90", "saves_per_90", "starts_per_90"],
-
-        "new_names": ["id", "first_name","second_name", "web_name", "team", "position", "selected_by_percent", "price","cost_change_start", "cost_change_start_fall",
-                    "value_form", "value_season", "total_points", "gw_points", "bps", "points_per_game", "expected_points_next", "expected_points_this", "form", "starts", "minutes",
-                    "goals", "xg", "assists", "xa", "xgi", "clean_sheets", "goals_conceded", "xgc",
-                    "own_goals", "penalties_saved", "penalties_missed", "yellow_cards", "red_cards", "saves", "bonus", "influence", "creativity", "threat", "ict_index",
-                    "xg_per_90", "xa_per_90", "clean_sheets_per_90", "xgi_per_90", "xgc_per_90", 
-                    "goals_conceded_per_90", "saves_per_90", "starts_per_90"]
-    }
-
-    # Create a dictionary of the teams and the correspoinding team codes for each of the players 
-    team_replace_dict = { 
-        1 : "Arsenal",
-        2 : "Aston Villa",
-        3 : "Bournemouth",
-        4 : "Brentford",
-        5 : "Brighton",
-        6 : "Burnley",
-        7 : "Chelsea",
-        8 : "Crystal Palace", 
-        9 : "Everton",
-        10 : "Fulham",
-        11 : "Liverpool",
-        12 : "Luton",
-        13 : "Man City",
-        14 : "Man Utd",
-        15 : "Newcastle",
-        16 : "Nott'm Forrest",
-        17 : "Sheffield Utd",
-        18 : "Spurs",
-        19 : "West Ham",
-        20 : "Wolves"
-    }
-
+import utility
+def scrape_data():
     # Create a dictionary of the position of the players in the raw format and the corrosponding position that that the players play in. 
     position_replace_dict = { 1: "GLK", 2: "DEF", 3 : "MID", 4: "FWD"}   
     
-    # Define a function that rearranges the columns within the c_df DataFrame
-    def test_function(current_column_name, new_column_name, column_position, dataframe):
-        reorder_col = dataframe.pop(current_column_name)
-        dataframe.insert(column_position, new_column_name, reorder_col)
-    
-    # Define a function to rename and rearrange column names for the two dataframes. 
-    def sort_columns(dataframe, col_dict, team_replace_dict, position_replace_dict):
-
-        # Loop through the length of the columns and call the test_function()
-        for i in range(len(col_dict["new_names"])):
-            test_function(
-                col_dict["old_names"][i],
-                col_dict["new_names"][i],
-                i,
-                dataframe
-            )
-            
-        # Replace the position from numbers to letters 
-        dataframe["position"].replace(position_replace_dict, inplace = True)
-
-        # Replace the teams from numbers to the full name of the club the players play for. 
-        dataframe["team"].replace(team_replace_dict, inplace = True)
-        return dataframe
-
     # Define API endpoint
     url = "https://fantasy.premierleague.com/api/bootstrap-static/"
 
-    try:
-        # Connect with the API and save the data as a .json()
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-        # Filter through the .json() for "elements" == "players"
-        players = data["elements"]
+    # Connect with the API and save the data as a .json()
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    # Filter through the .json() for "elements" == "players"
+    players = data["elements"]
 
-        # Convert the .json() into a pandas DataFrame for easier analysis. 
-        players_df = pd.DataFrame(players)
+    # Convert the .json() into a pandas DataFrame for easier analysis. 
+    players_df = pd.DataFrame(players)
 
-        # Save the DataFrame to .csv file for offline analysis 
-        players_df.to_csv("data/2023-2024/players_raw.csv", index = False)
+    # Save the DataFrame to .csv file for offline analysis 
+    players_df.to_csv("data/2023-2024/players_raw.csv", index = False)
 
-        # Create a new DataFrame that contains the wanted columns and unwanted columns
-        cleaned_df = players_df[df_columns["old_names"]]
+    # Create a DataFrame with only the id, Name, team and position and save to a .csv file for offline analysis
+    player_id_df = players_df[["first_name", "second_name", "id", "team"]]
+    player_id_df["team"].replace(utility.team_replace_dict, inplace = True)
+    player_id_df.to_csv("data/2023-2024/player_idlist.csv", index=False)
+
+    # Create a DataFrame to the same format as 2022-2023/cleaned_players.csv format for easy analysis and comparison to previous season.
+    cleaned_players_df = players_df[["first_name", "second_name", "goals_scored", "assists", "total_points",
+                                "minutes", "goals_conceded", "creativity", "influence", "threat", "bonus",
+                                "bps", "ict_index", "clean_sheets", "red_cards", "yellow_cards", "selected_by_percent",
+                                "now_cost", "element_type"]]
+
+    # Replace element_type numbers with corresponding positions
+    cleaned_players_df["element_type"].replace(position_replace_dict, inplace = True)
+
+    # Save the cleaned_players_df to a .csv file for offline analysis
+    cleaned_players_df.to_csv("data/2023-2024/cleaned_players.csv", index = False)
+
+
+def get_gw_data(player_id):
+        player_id_list = pd.read_csv("data/2023-2024/player_idlist.csv")
+        url = "https://fantasy.premierleague.com/api/"
+        endpoint = "element-summary/"
+
+        response = requests.get(url + endpoint+ str(player_id)+ "/").json()
+
+        df = pd.DataFrame(response["history"])
+
+        player_id = player_id_list[player_id_list["id"] == int(player_id)].reset_index()
+
+        # Obtain the player name
+        player_first_name = player_id.at[0,"first_name"]
+        player_second_name = player_id.at[0,"second_name"]
+
+        player_name = player_first_name + " " + player_second_name
+        team_name = player_id.at[0,"team"]
+
+        df["element"] = player_name
+        df.rename(columns={"element":"name"}, inplace=True)
+        df["team"] = team_name
+        return df 
+
+
+def get_extended_gw_data(player_id, GW_number):
+        # Define the endpoint to be used
+        url = "https://fantasy.premierleague.com/api/"
+        endpoint = "event/"+ str(GW_number) + "/live/"
+
+        # Request data from fpl apy
+        response = requests.get(url + endpoint).json()
+
+        # Convert json to dataframe filtering through "elements" (players)
+        player_df = pd.DataFrame(response["elements"])
         
-        # Create a new DataFrame that contains the wanted calumns and unwanted columns
-        other_df = players_df[other_columns["old_names"]]
+        # Filter and locate the desired player by using the player_id passed through the function
+        player_df = player_df[player_df["id"] == player_id].reset_index()
 
-        # Call upon the sort columns for both of the dataframes to rename columns and clean the dataframes. 
-        useful_player_stats_df = sort_columns(cleaned_df, df_columns ,team_replace_dict, position_replace_dict)
-        other_stats_df = sort_columns(other_df, other_columns, team_replace_dict, position_replace_dict)
+        # Extract the data from the "stats" column
+        player_df = player_df.at[0, "stats"]
+        player_df = pd.DataFrame(player_df, index =[0])
+        # Add a column called round with the value of the game week. This will be needed to merge the dataframe to the player_performance dataframe above
+        player_df["round"] = GW_number
 
-        # Create a DataFrame with only the id, Name, team and position and save to a .csv file for offline analysis
-        player_id_df = cleaned_df[["first_name", "second_name", "id"]]
-        player_id_df.to_csv("data/2023-2024/player_idlist.csv", index=False)
+        # Return the player dataframe
+        return player_df
 
-        # Create a DataFrame to the same format as 2022-2023/cleaned_players.csv format for easy analysis and comparison to previous season.
-        cleaned_players_df = players_df[["first_name", "second_name", "goals_scored", "assists", "total_points",
-                                        "minutes", "goals_conceded", "creativity", "influence", "threat", "bonus",
-                                        "bps", "ict_index", "clean_sheets", "red_cards", "yellow_cards", "selected_by_percent",
-                                        "now_cost", "element_type"]]
-        
-        # Replace element_type numbers with corresponding positions
-        cleaned_players_df["element_type"].replace(position_replace_dict, inplace = True)
-        
-        # Save the cleaned_players_df to a .csv file for offline analysis
-        cleaned_players_df.to_csv("data/2023-2024/cleaned_players.csv", index = False)
 
-        return useful_player_stats_df, other_stats_df
+def get_all_gw_data():
+    # Load player_idlist.csv
+    player_id_list = pd.read_csv("data/2023-2024/player_idlist.csv")
+    merged_gw = pd.DataFrame()
+
+    for i, id in enumerate(player_id_list["id"]):
+        print("Loading Player: "+ str(i) + "/" + str(len(player_id_list)-1))
+        df = get_gw_data(id)
+
+        merged_gw = pd.concat([merged_gw, df])
     
-    # If there is an error with the HTML request a previously saved DataFrame is loaded instead
-    except requests.exceptions.RequestException as e:
-        print("An error occured:", e )
+    merged_gw.to_csv("data/2023-2024/merged_gw.csv", index = False)
 
-        file_path = "data/2023-2024/players_raw.csv"
-        def check_offline_file(file_path):
-            return os.path.exists(file_path)
-        
-        if check_offline_file(file_path):
-            print("Loading locally saved database")
-            players_df = pd.read_csv("data/2023-2024/players_raw.csv")
-
-            # Create a new DataFrame that contains the wanted columns and unwanted columns
-            cleaned_df = players_df[df_columns["old_names"]]
-            
-            # Create a new DataFrame that contains the wanted calumns and unwanted columns
-            other_df = players_df[other_columns["old_names"]]
-
-            # Call upon the sort columns for both of the dataframes to rename columns and clean the dataframes. 
-            useful_player_stats_df = sort_columns(cleaned_df, df_columns ,team_replace_dict, position_replace_dict)
-            other_stats_df = sort_columns(other_df, other_columns, team_replace_dict, position_replace_dict)
-
-            return useful_player_stats_df, other_stats_df
-        
-        else:
-            print("No offline player Database available to load")
-
-
-
+    
 def format_historical_csv():
     season_list = ["2016-2017", "2017-2018", "2018-2019", "2019-2020", "2020-2021", "2021-2022", "2022-2023", "2023-2024"]
     for season in season_list:
