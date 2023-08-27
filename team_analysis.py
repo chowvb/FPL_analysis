584bbc88e6cb4b8813b6a63f269fb9726e54d166
Vincent Chow <chowvk8@gmail.com>
2023-08-27 10:08:26 +0100

    Changed clean_team_data() functions output to change short name to full team name, reordered team strength to be in the correct orientation for the octagon plot labels

--- team_analysis.py
+++ team_analysis.py
@@ -52,12 +52,14 @@ def clean_team_data(season):
         defence_overall.append(np.round(defence))
 
     # Filter and pull out the relavent strength stats from the team_df into a new DataFrame
-    team_strength_stat = team_df[["short_name", "strength_overall_home", "strength_overall_away", "strength_attack_home", "strength_attack_away", "strength_defence_home", "strength_defence_away"]]
+    team_strength_stat = team_df[["name", "strength_overall_home", "strength_overall_away", "strength_attack_home", "strength_attack_away", "strength_defence_home", "strength_defence_away"]]
     
     # Add the two new lists as new columns in the DataFrame
     team_strength_stat["attack_overall"] = attack_overall
     team_strength_stat["defence_overall"] = defence_overall
     
+    team_strength_stat = team_strength_stat.reindex(columns=["name", "strength_overall_away","strength_attack_away", "attack_overall", "strength_attack_home",
+                                                             "strength_overall_home", "strength_defence_home", "defence_overall", "strength_defence_away"])
     # Save the team strength database into a new .csv file
     team_strength_stat.to_csv("data/"+ season + "/teams_strength_stat.csv", index = False)
 
@@ -155,3 +157,71 @@ def get_player_roster(team_id):
     
     # Return the roster to the user
     return player_roster
+
+
+def h2h_results(team, opp_team):
+    """
+    ARGS: 
+        - team (string) the team you wish to analyse.
+        - opp_team (string) opponent team you wish to compare with.
+    RETURNS:
+        - DataFrame with recent matchups (only Premier League matches that date back to 2020)
+    EXAMPLE:
+        h2h_results("Liverpool", "Newcastle")
+    """
+    merged_season_df = pd.read_csv("data/cleaned_merged_seasons.csv", low_memory= False)
+
+    team_results = merged_season_df[merged_season_df["team_x"] == team]
+    team_results = team_results[["kickoff_time", "opp_team_name", "was_home", "team_a_score", "team_h_score"]]
+
+    # Group duplicate rows
+    list_of_columns = ['kickoff_time']  # List of columns that determine duplicates
+    column_name = 'kickoff_time'  # Column to merge
+    grouped = team_results.groupby(list_of_columns).apply(lambda x: ' '.join(x[column_name]))
+
+    # Create new DataFrame
+    new_df = pd.DataFrame(grouped, columns=[column_name])
+
+    # Remove duplicate rows from the original DataFrame
+    df = team_results.drop_duplicates(subset=list_of_columns)
+
+    # Merge DataFrames
+    final_df = pd.concat([df, new_df], ignore_index=True)
+
+    # Remove matches with NA values
+    final_df.dropna(inplace = True)
+
+
+    sorted_score = []
+
+    for i, other in enumerate(final_df["was_home"]):
+
+        if other == True:
+            sorted_score.append({"score": final_df.at[i, "team_h_score"], "opp_score": final_df.at[i,"team_a_score"]})
+            
+        else:
+            sorted_score.append({"score": final_df.at[i, "team_a_score"], "opp_score": final_df.at[i,"team_h_score"]})
+
+    result = pd.DataFrame(sorted_score)
+    is_win = []
+
+    for i, score in enumerate(result["score"]):
+        if score > result.at[i, "opp_score"]:
+            is_win.append("W")
+        elif score < result.at[i,"opp_score"]:
+            is_win.append("L")
+        else:
+            is_win.append("D")
+
+    result["result"] = is_win
+
+
+    test_df = pd.concat([final_df, result],  axis = 1)
+    test_df = test_df.drop(columns={"team_a_score","team_h_score"})
+    test_df["team"] = team
+
+    df_order = ["kickoff_time","was_home", "team", "score", "opp_score", "opp_team_name", "result"]
+
+    test_df = test_df.reindex(columns=df_order)
+    h2h = test_df[test_df["opp_team_name"] == opp_team]
+    return h2h
